1308b3a6593c51a742a7365a58d9a53fa6550482
Александра <112776816+SaiALL441@users.noreply.github.com>
2023-03-02 20:28:26 +0200

    Add files via upload

---
+++ lab2-3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualBasic.FileIO;
+
+namespace lab2_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (TextFieldParser tfp = new TextFieldParser(@"D:\2 курс"))
+            {
+                tfp.TextFieldType = FieldType.Delimited;
+                tfp.SetDelimiters(",");
+
+                while (!tfp.EndOfData)
+                {
+                    string[] fields = tfp.ReadFields();
+                }
+            }
+        }
+    }
+}
